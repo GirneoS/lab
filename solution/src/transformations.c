7aeff93b828105fc0f7b3777b0b1f6a81c83c8da
aleksaha <aleksaha@niuitmo.ru>
2024-12-29 12:59:27 +0300

    change transform functions signature

--- solution/src/transformations.c
+++ solution/src/transformations.c
@@ -2,71 +2,71 @@
 
 #include "image.h"
 
-void none(struct image* init_img, struct image* transformed_img){
-    transformed_img->height = init_img->height;
-    transformed_img->width = init_img->width;
+void none(struct image* init_pic, struct image* final_pic){
+    final_pic->height = init_pic->height;
+    final_pic->width = init_pic->width;
 
-    transformed_img->data = malloc(init_img->height * init_img->width * sizeof(struct pixel));
+    final_pic->data = malloc(init_pic->height * init_pic->width * sizeof(struct pixel));
 
-    for (int64_t i = 0; i < transformed_img->height; i++) {
-        for (int64_t j = 0; j < transformed_img->width; j++) {
-            transformed_img->data[i * transformed_img->width + j] = init_img->data[i * init_img->width + j];
+    for (int64_t i = 0; i < final_pic->height; i++) {
+        for (int64_t j = 0; j < final_pic->width; j++) {
+            final_pic->data[i * final_pic->width + j] = init_pic->data[i * init_pic->width + j];
         }
     }
 }
 
-void flip_h(struct image* init_img, struct image* transformed_img){
-    transformed_img->height = init_img->height;
-    transformed_img->width = init_img->width;
+void flip_h(struct image* init_pic, struct image* final_pic){
+    final_pic->height = init_pic->height;
+    final_pic->width = init_pic->width;
 
-    transformed_img->data = malloc(transformed_img->height * transformed_img->width * sizeof(struct pixel));
+    final_pic->data = malloc(final_pic->height * final_pic->width * sizeof(struct pixel));
 
-    for (int64_t i = 0; i < init_img->height; i++) {
-        for (int64_t j = 0; j < init_img->width; j++) {
-            transformed_img->data[i * transformed_img->width + (init_img->width - 1 - j)] =
-                    init_img->data[i * init_img->width + j];
+    for (int64_t i = 0; i < init_pic->height; i++) {
+        for (int64_t j = 0; j < init_pic->width; j++) {
+            final_pic->data[i * final_pic->width + (init_pic->width - 1 - j)] =
+                    init_pic->data[i * init_pic->width + j];
         }
     }
 }
 
-void flip_v(struct image* init_image, struct image* transformed_img){
-    transformed_img->height = init_image->height;
-    transformed_img->width = init_image->width;
+void flip_v(struct image* init_pic, struct image* final_pic){
+    final_pic->height = init_image->height;
+    final_pic->width = init_image->width;
 
-    transformed_img->data = malloc(transformed_img->height * transformed_img->width * sizeof(struct pixel));
+    final_pic->data = malloc(final_pic->height * final_pic->width * sizeof(struct pixel));
 
     for (int64_t i = 0; i < init_image->height; i++) {
         for (int64_t j = 0; j < init_image->width; j++) {
-            transformed_img->data[(init_image->height - 1 - i) * transformed_img->width + j] =
+            final_pic->data[(init_image->height - 1 - i) * final_pic->width + j] =
                     init_image->data[i * init_image->width + j];
         }
     }
 }
 
-void cw_90(struct image* init_img, struct image* transformed_img){
-    transformed_img->width = init_img->height;
-    transformed_img->height = init_img->width;
+void cw_90(struct image* init_pic, struct image* final_pic){
+    final_pic->width = init_pic->height;
+    final_pic->height = init_pic->width;
 
-    transformed_img->data = malloc(transformed_img->height * transformed_img->width * sizeof(struct pixel));
+    final_pic->data = malloc(final_pic->height * final_pic->width * sizeof(struct pixel));
 
-    for (int64_t i = 0; i < init_img->height; i++) {
-        for (int64_t j = 0; j < init_img->width; j++) {
-            transformed_img->data[j * transformed_img->width + i] =
-                    init_img->data[i * init_img->width + (init_img->width - 1 - j)];
+    for (int64_t i = 0; i < init_pic->height; i++) {
+        for (int64_t j = 0; j < init_pic->width; j++) {
+            final_pic->data[j * final_pic->width + i] =
+                    init_pic->data[i * init_pic->width + (init_pic->width - 1 - j)];
         }
     }
 }
 
-void ccw_90(struct image* init_img, struct image* transformed_img){
-    transformed_img->width = init_img->height;
-    transformed_img->height = init_img->width;
+void ccw_90(struct image* init_pic, struct image* final_pic){
+    final_pic->width = init_pic->height;
+    final_pic->height = init_pic->width;
 
-    transformed_img->data = malloc(transformed_img->width * transformed_img->height * sizeof(struct pixel));
+    final_pic->data = malloc(final_pic->width * final_pic->height * sizeof(struct pixel));
 
-    for (int64_t i = 0; i < init_img->height; i++) {
-        for (int64_t j = 0; j < init_img->width; j++) {
-            transformed_img->data[j * transformed_img->width + init_img->height - 1 - i] =
-                    init_img->data[i * init_img->width + j];
+    for (int64_t i = 0; i < init_pic->height; i++) {
+        for (int64_t j = 0; j < init_pic->width; j++) {
+            final_pic->data[j * final_pic->width + init_pic->height - 1 - i] =
+                    init_pic->data[i * init_pic->width + j];
         }
     }
 }
